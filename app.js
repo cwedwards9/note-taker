d9791a02a234e1a3c9e0e07d48527133c0cf51d2
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-05 14:34:56 -0500

    Require path module and add middleware for static files and router() middleware

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+const path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 
-require("./routes/htmlRoutes")(app);
+// Serve static files (css and index.js file which handles front-end manipulation)
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Router() middleware for /api routes
+app.use("/api", apiRoutes);
+
+
+require("./routes/htmlRoutes")(app);
 
 
 app.listen(PORT, () => {
