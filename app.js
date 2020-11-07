3c133dfadc4de01d0e814b8a6d9e8950a58f64b9
Chase Edwards <chase.w.edwards9@gmail.com>
2020-11-06 19:04:46 -0500

    Remove path from requirements. Refactor code for using static files.

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
-const path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files (css and index.js file which handles front-end manipulation)
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static css and js files
+app.use(express.static(__dirname + '/public'));
 
 // Router() middleware for /api routes
 app.use("/api", apiRoutes);
-
 
 require("./routes/htmlRoutes")(app);
 
